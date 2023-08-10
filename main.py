e4d5372e71e4a16e1231fd6c8a2d68fb087089cd
Muhammad Nadeem <106726589+nadeem-cpp@users.noreply.github.com>
2023-08-10 12:39:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from linkedin import Linkedin
+import csv
 
-
+job_title = "python developer"
 def main():
     link = Linkedin()
-    link.search_job(title="python developer")
-    link.jobs_processing()
+    # search job with given titles
+    link.search_job(title=job_title)
+    # get jobs details from searched jobs
+    jobs = link.jobs_processing()
+    if jobs is not None:
+        link.quit()
+        with open('job_details.csv', 'w') as file:
+
+            data = csv.DictWriter(file, jobs.keys())
+            data.writeheader()
+            data.writerow(jobs)
 
 
 if __name__ == '__main__':
